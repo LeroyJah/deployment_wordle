08b4860ed0dbe69b4972988cb9e65bd0880103d4
LeroyJah <roy_animo@hotmail.com>
2023-07-04 00:04:39 +0200

    created db.json array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const[solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('  http://localhost:3001/solutions')
+        .then(res =>res.json())
+        .then(json => {
+          console.log(json)
+        })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+            <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Master Jah. Edit <code>src/App.js</code> and save to reload.
